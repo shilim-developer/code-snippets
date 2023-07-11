8d5306393de1af5d872a04f761592f2c87f14dc9
shilim <1251314045@qq.com>
2023-07-12 00:13:26 +0800

    refactor: update base

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { searchProPlugin } from "vuepress-plugin-search-pro";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/code-snippets/",
 
   lang: "zh-CN",
   title: "SLBlog代码段",
