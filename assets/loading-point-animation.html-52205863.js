611266b3b79cc28829718fb6a7a0f9e8b5387b88
shilim-developer <1251314045@qq.com>
2024-07-22 01:44:22 +0000

    Deploying to gh-pages from @ shilim-developer/code-snippets@1fbdc06f9a64d40caa33437e6d393ff312685d13 🚀

---
+++ assets/loading-point-animation.html-52205863.js
@@ -0,0 +1,33 @@
+import{_ as n}from"./plugin-vue_export-helper-c27b6911.js";import{o as s,c as a,b as t}from"./app-a2aa737d.js";const p={},e=t(`<h2 id="加载中小点点动画" tabindex="-1"><a class="header-anchor" href="#加载中小点点动画" aria-hidden="true">#</a> 加载中小点点动画</h2><div class="language-html line-numbers-mode" data-ext="html"><pre class="language-html"><code><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>html</span><span class="token punctuation">&gt;</span></span>
+  <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>style</span><span class="token punctuation">&gt;</span></span><span class="token style"><span class="token language-css">
+    <span class="token selector">button</span> <span class="token punctuation">{</span>
+      <span class="token property">background-color</span><span class="token punctuation">:</span> black<span class="token punctuation">;</span>
+      <span class="token property">color</span><span class="token punctuation">:</span> #ffffff<span class="token punctuation">;</span>
+      <span class="token property">padding</span><span class="token punctuation">:</span> 6px 20px<span class="token punctuation">;</span>
+      <span class="token property">padding-right</span><span class="token punctuation">:</span> 26px<span class="token punctuation">;</span>
+      <span class="token property">border</span><span class="token punctuation">:</span> none<span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+    <span class="token selector">.loading</span> <span class="token punctuation">{</span>
+      <span class="token property">height</span><span class="token punctuation">:</span> 2px<span class="token punctuation">;</span>
+      <span class="token property">width</span><span class="token punctuation">:</span> 2px<span class="token punctuation">;</span>
+      <span class="token property">display</span><span class="token punctuation">:</span> inline-block<span class="token punctuation">;</span>
+      <span class="token property">animation</span><span class="token punctuation">:</span> loading-animation 2.4s infinite <span class="token function">steps</span><span class="token punctuation">(</span>1<span class="token punctuation">,</span> start<span class="token punctuation">)</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+    <span class="token atrule"><span class="token rule">@keyframes</span> loading-animation</span> <span class="token punctuation">{</span>
+      <span class="token selector">25%</span> <span class="token punctuation">{</span>
+        <span class="token property">box-shadow</span><span class="token punctuation">:</span> 2px 0 0 white<span class="token punctuation">;</span>
+      <span class="token punctuation">}</span>
+      <span class="token selector">50%</span> <span class="token punctuation">{</span>
+        <span class="token property">box-shadow</span><span class="token punctuation">:</span> 2px 0 0 white<span class="token punctuation">,</span> 7px 0 0 white<span class="token punctuation">;</span>
+      <span class="token punctuation">}</span>
+      <span class="token selector">75%</span> <span class="token punctuation">{</span>
+        <span class="token property">box-shadow</span><span class="token punctuation">:</span> 2px 0 0 white<span class="token punctuation">,</span> 7px 0 0 white<span class="token punctuation">,</span> 12px 0 0 white<span class="token punctuation">;</span>
+      <span class="token punctuation">}</span>
+    <span class="token punctuation">}</span>
+  </span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>style</span><span class="token punctuation">&gt;</span></span>
+  <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>button</span><span class="token punctuation">&gt;</span></span>
+    加载中
+    <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>span</span> <span class="token attr-name">class</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>loading<span class="token punctuation">&quot;</span></span><span class="token punctuation">&gt;</span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>span</span><span class="token punctuation">&gt;</span></span>
+  <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>button</span><span class="token punctuation">&gt;</span></span>
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>html</span><span class="token punctuation">&gt;</span></span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),o=[e];function c(l,i){return s(),a("div",null,o)}const d=n(p,[["render",c],["__file","loading-point-animation.html.vue"]]);export{d as default};
